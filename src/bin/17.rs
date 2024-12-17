ca5830b4a6ed18d28f71d350805fd94e3d7a1e93
plastic041 <plastic041@gmail.com>
2024-12-17 17:25:53 +0900

    17

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,6 +1,5 @@
 use core::panic;
 use itertools::Itertools;
-use rayon::prelude::*;
 
 advent_of_code::solution!(17);
 
@@ -55,7 +54,6 @@ pub fn part_one(input: &str) -> Option<String> {
                     6 => reg_c,
                     _ => panic!("invalid combo"),
                 };
-                println!("adv: a = {} / {}", reg_a, 2u32.pow(combo_value));
                 reg_a /= 2u32.pow(combo_value);
 
                 pointer += 2;
@@ -63,7 +61,6 @@ pub fn part_one(input: &str) -> Option<String> {
             1 => {
                 let literal = program[pointer + 1];
                 reg_b ^= literal;
-                println!("bxl: b = {} ^ {}", reg_b, literal);
 
                 pointer += 2;
             }
@@ -79,23 +76,19 @@ pub fn part_one(input: &str) -> Option<String> {
                     6 => reg_c,
                     _ => panic!("invalid combo"),
                 };
-                println!("bst: b = {} mod {}", combo_value, 8);
                 reg_b = combo_value.rem_euclid(8);
 
                 pointer += 2;
             }
             3 => {
                 if reg_a == 0 {
-                    println!("jnz, a: {}", reg_a);
                     pointer += 2;
                 } else {
                     let literal = program[pointer + 1];
-                    println!("jnz, a: {} -> jump to {}", reg_a, literal);
                     pointer = literal as usize;
                 }
             }
             4 => {
-                println!("bxc, b = {} ^ {}", reg_b, reg_c);
                 reg_b ^= reg_c;
 
                 pointer += 2;
@@ -112,7 +105,6 @@ pub fn part_one(input: &str) -> Option<String> {
                     6 => reg_c,
                     _ => panic!("invalid combo"),
                 };
-                println!("out, {} mod 8", combo_value);
                 outputs.push(combo_value.rem_euclid(8));
 
                 pointer += 2;
@@ -130,7 +122,6 @@ pub fn part_one(input: &str) -> Option<String> {
                     6 => reg_c,
                     _ => panic!("invalid combo"),
                 };
-                println!("bdv, b = {} / {}", reg_a, 2u32.pow(combo_value));
                 reg_b = reg_a / 2u32.pow(combo_value);
 
                 pointer += 2;
@@ -148,7 +139,6 @@ pub fn part_one(input: &str) -> Option<String> {
                     6 => reg_c,
                     _ => panic!("invalid combo"),
                 };
-                println!("cdv, c = {} / {}", reg_a, 2u32.pow(combo_value));
                 reg_c = reg_a / 2u32.pow(combo_value);
 
                 pointer += 2;
@@ -160,7 +150,7 @@ pub fn part_one(input: &str) -> Option<String> {
     Some(outputs.into_iter().join(","))
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<usize> {
     let (registers, programs) = input.split("\n\n").collect_tuple().unwrap();
     let mut register_lines = registers.lines();
     register_lines.next();
@@ -169,162 +159,162 @@ pub fn part_two(input: &str) -> Option<u32> {
         .unwrap()
         .split(": ")
         .collect::<Vec<_>>()[1]
-        .parse::<u32>()
+        .parse()
         .unwrap();
     let reg_c_def = register_lines
         .next()
         .unwrap()
         .split(": ")
         .collect::<Vec<_>>()[1]
-        .parse::<u32>()
+        .parse()
         .unwrap();
 
-    let program: Vec<u32> = programs.split(": ").collect::<Vec<_>>()[1]
+    let program: Vec<usize> = programs.split(": ").collect::<Vec<_>>()[1]
         .split(",")
-        .map(|s| s.parse::<u32>().unwrap())
+        .map(|s| s.parse().unwrap())
         .collect();
 
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        .map(|mut n| {
-            n += 111200000;
-            n
-        })
-        .into_par_iter()
-        .try_for_each(|start| {
-            let mut reg_a_iter = start;
-            loop {
-                let mut pointer = 0;
-                let mut reg_a = reg_a_iter;
-                let mut reg_b = reg_b_def;
-                let mut reg_c = reg_c_def;
+    let mut reg_a_iter = 0;
+    loop {
+        let mut pointer = 0;
+        let mut reg_a = reg_a_iter;
+        let mut reg_b = reg_b_def;
+        let mut reg_c = reg_c_def;
 
-                let mut outputs = vec![];
+        let mut outputs = Vec::with_capacity(program.len());
 
-                while let Some(opcode) = program.get(pointer) {
-                    if pointer + 1 >= program.len() {
-                        break;
-                    }
-                    match opcode {
-                        0 => {
-                            // adv
-                            let combo = program[pointer + 1];
-                            let combo_value = match combo {
-                                0 => 0,
-                                1 => 1,
-                                2 => 2,
-                                3 => 3,
-                                4 => reg_a,
-                                5 => reg_b,
-                                6 => reg_c,
-                                _ => panic!("invalid combo"),
-                            };
-                            reg_a /= 2u32.pow(combo_value);
-
-                            pointer += 2;
-                        }
-                        1 => {
-                            let literal = program[pointer + 1];
-                            reg_b ^= literal;
-
-                            pointer += 2;
-                        }
-                        2 => {
-                            let combo = program[pointer + 1];
-                            let combo_value = match combo {
-                                0 => 0,
-                                1 => 1,
-                                2 => 2,
-                                3 => 3,
-                                4 => reg_a,
-                                5 => reg_b,
-                                6 => reg_c,
-                                _ => panic!("invalid combo"),
-                            };
-                            reg_b = combo_value.rem_euclid(8);
-
-                            pointer += 2;
-                        }
-                        3 => {
-                            if reg_a == 0 {
-                                pointer += 2;
-                            } else {
-                                let literal = program[pointer + 1];
-                                pointer = literal as usize;
-                            }
-                        }
-                        4 => {
-                            reg_b ^= reg_c;
-
-                            pointer += 2;
-                        }
-                        5 => {
-                            let combo = program[pointer + 1];
-                            let combo_value = match combo {
-                                0 => 0,
-                                1 => 1,
-                                2 => 2,
-                                3 => 3,
-                                4 => reg_a,
-                                5 => reg_b,
-                                6 => reg_c,
-                                _ => panic!("invalid combo"),
-                            };
-                            outputs.push(combo_value.rem_euclid(8));
-
-                            pointer += 2;
-                        }
-                        6 => {
-                            // bdv
-                            let combo = program[pointer + 1];
-                            let combo_value = match combo {
-                                0 => 0,
-                                1 => 1,
-                                2 => 2,
-                                3 => 3,
-                                4 => reg_a,
-                                5 => reg_b,
-                                6 => reg_c,
-                                _ => panic!("invalid combo"),
-                            };
-                            reg_b = reg_a / 2u32.pow(combo_value);
-
-                            pointer += 2;
-                        }
-                        7 => {
-                            // cdv
-                            let combo = program[pointer + 1];
-                            let combo_value = match combo {
-                                0 => 0,
-                                1 => 1,
-                                2 => 2,
-                                3 => 3,
-                                4 => reg_a,
-                                5 => reg_b,
-                                6 => reg_c,
-                                _ => panic!("invalid combo"),
-                            };
-                            reg_c = reg_a / 2u32.pow(combo_value);
-
-                            pointer += 2;
-                        }
-                        _ => panic!("invalid opcode"),
-                    }
-                }
-
-                if outputs == program {
-                    println!("tried reg_a == {}", reg_a_iter);
-                    return None;
-                } else {
-                    if reg_a_iter % 1000000 == 0 {
-                        println!("tried reg_a == {}", reg_a_iter);
-                    }
-                    reg_a_iter += 10;
-                }
+        while let Some(opcode) = program.get(pointer) {
+            if pointer + 1 >= program.len() {
+                break;
             }
-        });
+            match opcode {
+                0 => {
+                    // adv
+                    let combo = program[pointer + 1];
+                    let combo_value = match combo {
+                        0 => 0,
+                        1 => 1,
+                        2 => 2,
+                        3 => 3,
+                        4 => reg_a,
+                        5 => reg_b,
+                        6 => reg_c,
+                        _ => panic!("invalid combo"),
+                    };
+                    reg_a /= 2usize.pow(combo_value as u32);
 
-    // Some(reg_a_iter)
-    None
+                    pointer += 2;
+                }
+                1 => {
+                    reg_b ^= program[pointer + 1];
+
+                    pointer += 2;
+                }
+                2 => {
+                    let combo = program[pointer + 1];
+                    let combo_value = match combo {
+                        0 => 0,
+                        1 => 1,
+                        2 => 2,
+                        3 => 3,
+                        4 => reg_a,
+                        5 => reg_b,
+                        6 => reg_c,
+                        _ => panic!("invalid combo"),
+                    };
+                    reg_b = combo_value % 8;
+
+                    pointer += 2;
+                }
+                3 => {
+                    if reg_a == 0 {
+                        pointer += 2;
+                    } else {
+                        pointer = program[pointer + 1];
+                    }
+                }
+                4 => {
+                    reg_b ^= reg_c;
+
+                    pointer += 2;
+                }
+                5 => {
+                    let combo = program[pointer + 1];
+                    let combo_value = match combo {
+                        0 => 0,
+                        1 => 1,
+                        2 => 2,
+                        3 => 3,
+                        4 => reg_a,
+                        5 => reg_b,
+                        6 => reg_c,
+                        _ => panic!("invalid combo"),
+                    };
+                    outputs.push(combo_value % 8);
+
+                    pointer += 2;
+                }
+                6 => {
+                    // bdv
+                    let combo = program[pointer + 1];
+                    let combo_value = match combo {
+                        0 => 0,
+                        1 => 1,
+                        2 => 2,
+                        3 => 3,
+                        4 => reg_a,
+                        5 => reg_b,
+                        6 => reg_c,
+                        _ => panic!("invalid combo"),
+                    };
+                    reg_b = reg_a / 2usize.pow(combo_value as u32);
+
+                    pointer += 2;
+                }
+                7 => {
+                    // cdv
+                    let combo = program[pointer + 1];
+                    let combo_value = match combo {
+                        0 => 0,
+                        1 => 1,
+                        2 => 2,
+                        3 => 3,
+                        4 => reg_a,
+                        5 => reg_b,
+                        6 => reg_c,
+                        _ => panic!("invalid combo"),
+                    };
+                    reg_c = reg_a / 2usize.pow(combo_value as u32);
+
+                    pointer += 2;
+                }
+                _ => panic!("invalid opcode"),
+            }
+        }
+
+        if outputs == program {
+            println!("reg_a == {}", reg_a_iter);
+            return Some(reg_a_iter);
+        } else {
+            let p = check_pos(&outputs, &program);
+            reg_a_iter += 8usize.pow(p as u32);
+        }
+    }
+}
+
+fn check_pos(output: &[usize], program: &[usize]) -> usize {
+    if output.len() == program.len() {
+        for i in 0..program.len() {
+            let pos = program.len() - i - 1;
+            let o = output[pos];
+            let p = program[pos];
+            if o != p {
+                return pos;
+            }
+        }
+    }
+
+    program.len() - output.len()
 }
 
 #[cfg(test)]
